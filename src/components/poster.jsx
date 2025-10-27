f7faec71fccb4df9e688a4f339824cc53311544a
germaniii <germaniiifelisarta@gmail.com>
2025-10-27 13:46:13 +0800

    Add overview when hovering

--- src/components/poster.jsx
+++ src/components/poster.jsx
@@ -1,6 +1,7 @@
 const Poster = ({ details }) => {
   const id = details.id ?? "";
   const title = details.title ?? "";
+  const overview = details.overview ?? "";
   const releaseDate = details.release_date ?? "";
   const posterPath = details.poster_path ?? "";
 
@@ -8,7 +9,7 @@ const Poster = ({ details }) => {
     <div
       key={id}
       className="
-        cursor-pointer
+        group cursor-pointer
         relative rounded-2xl overflow-hidden bg-gray-800 text-white shadow-lg
         transform transition-all hover:scale-103
         w-full
@@ -21,10 +22,13 @@ const Poster = ({ details }) => {
         minHeight: "400px",
       }}
     >
-      <div className="absolute inset-0 bg-linear-to-b from-70% to-blue-700" />
+      <div className="absolute inset-0 bg-linear-to-b from-70% to-blue-700 group-hover:from-30%" />
       <div className="absolute bottom-0 p-4 w-full text-left">
         <h3 className="text-xl font-bold">{title}</h3>
         <p className="text-sm opacity-80">{releaseDate}</p>
+        <p className="hidden group-hover:block transform transition-all text-ellipsis overflow-scroll max-h-50">
+          {overview}
+        </p>
       </div>
     </div>
   );
